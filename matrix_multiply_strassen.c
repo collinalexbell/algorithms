ee921df0454e9b052ceff5a1e7475459f395fb59
kuberlog <collinalexbell@gmail.com>
2020-08-19 07:20:05 -0500

    matrix_multiply_strassen.c: copied over the recursive and made changes:
    - deleted core of matrix_multiply
    - changed matrix_add to accept a 3rd array to copy into
    - derived matrix_subtract from new matrix_add

--- matrix_multiply_strassen.c
+++ matrix_multiply_strassen.c
@@ -4,8 +4,68 @@
 
 #define MAX_N 1000000
 
-void matrix_multiply(int n, int A[n][n], int B[n][n], int C[n][n]) {
-	
+int** create_array(int n) {
+    int* values = calloc(n*n, sizeof(int));
+    int** rows = malloc(n*sizeof(int*));
+    for (int i=0; i<n; ++i)
+    {
+        rows[i] = values + i*n;
+    }
+    return rows;
+}
+
+void destroy_array(int** arr)
+{
+    free(*arr);
+    free(arr);
+}
+
+struct Point {
+	int row;
+	int col;
+};
+
+struct Point pointAdd(struct Point p, int row, int col) {
+  struct Point rv = {p.row + row, p.col + col};
+  return rv;
+}
+
+void matrix_copy(int** C, int** to_copy, struct Point start, int n) {
+  int row, col;
+  for(row = 0; row < n; row++) {
+    for(col = 0; col < n; col++) {
+      C[row + start.row][col + start.col] = to_copy[row][col];
+    }
+  }
+}
+
+void matrix_add(int** A, int** B, int** C, int n) {
+  int row, col;
+  for(row = 0; row < n; row++) {
+    for(col = 0; col < n; col++) {
+      C[row][col] = A[row][col] + B[row][col];
+    }
+  }
+}
+
+void matrix_subtract(int** A, int** B, int** C, int n) {
+  int row, col;
+  for(row = 0; row < n; row++) {
+    for(col = 0; col < n; col++) {
+      C[row][col] = A[row][col] - B[row][col];
+    }
+  }
+}
+
+int** matrix_multiply(int** A, int** B, struct Point startA, struct Point startB, int n) {
+	int** C = create_array(n);
+	if(n == 1) {
+		C[0][0] = A[startA.row][startA.col] * B[startB.row][startB.col];
+		return C;
+	}
+
+  int mid = n/2;
+	return C;
 }
 
 int main(int argc, char** argv) {
@@ -17,14 +77,16 @@ int main(int argc, char** argv) {
 		exit(1);
 	}
 
-	n = atoi(argv[1]);	
+	n = atoi(argv[1]);
 	if(n < 0 || n > MAX_N) {
 		fprintf(stderr,"invalid n:%d for (n x n)	 matrix", n);
 	}
 
-	int A[n][n];
-	int B[n][n];
-	int C[n][n];
+	int** A = create_array(n);
+	struct Point startA = {0,0};
+	struct Point startB = {0,0};
+	int** B = create_array(n);
+	int** C;
 
 
 	for(i = 0; i < n; i++) {
@@ -44,7 +106,8 @@ int main(int argc, char** argv) {
 		exit(1);
 	}
 
-	matrix_multiply(n, A, B, C);
+
+	C = matrix_multiply(A, B, startA, startB, n);
 
 	for(i = 0; i < n; i++) {
 		for(j = 0; j < n; j++) {
