e763c18e1835560de6652f6269fc429adaf830c1
kuberlog <collinalexbell@gmail.com>
2022-10-22 19:46:32 +0000

    algos are easy you hackers

---
+++ gensort.lisp
@@ -0,0 +1,16 @@
+
+(defun main (args)
+  (let (( result (let (
+               (n 0)
+               (r 10000))
+           (if (>= (length args) 1)
+               (setf n (nth 0 args))
+               (return-from main nil))
+           (if (>= (length args) 2)
+               (setf r (nth 1 args)))
+           (loop for i from 0 to n collect
+             (let ((sign (if (eq (random 2) 1) -1 1)))
+               (format nil "~s" (* (random r) sign)))))))
+    (remove-if (lambda (a) (eq a #\-)) result)))
+
+
